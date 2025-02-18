d9bedb0433f64be7ba46f6210f0eca8deada5b8d
Aaron Piotrowski <aaron@artificialpixel.com>
2025-02-18 14:28:20 -0500

    **Remove About page and related resources.**
    The About page, its associated styles, and imports have been deleted. This cleanup removes unnecessary components and CSS, streamlining the project's file structure.

--- Data/Event.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChapelHillDotNet.Data;
-
-public class Event
-{
-    public required string Title { get; set; }
-    public string? Description { get; set; }
-    public DateTime Date { get; set; }
-    public required string Location { get; set; }
-}
--- Data/Feature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChapelHillDotNet.Data;
-
-public class Feature
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string IconColor { get; set; }
-    public string IconName { get; set; }
-}

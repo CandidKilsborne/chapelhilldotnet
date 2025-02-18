8f419f0a4f1734b097a30ec8ec6c3ef2200137a6
Aaron Piotrowski <aaron@artificialpixel.com>
2025-02-18 14:58:18 -0500

    **Switch to Blazicons and enhance event model.**
    Replaced various icon components with Blazicons for consistency and updated dependencies accordingly. Extended the `Event` model to include `Id`, `Time`, and `Attendees` properties, and adjusted CSS with a new `.w-half` class for better layout control.

--- chapelhilldotnet.web/Models/Event.cs
+++ chapelhilldotnet.web/Models/Event.cs
@@ -2,8 +2,11 @@
 
 public class Event
 {
+    public int Id { get; set; }
     public required string Title { get; set; }
     public string? Description { get; set; }
     public DateTime Date { get; set; }
     public required string Location { get; set; }
+    public string Time { get; set; } = string.Empty;
+    public int Attendees { get; set; }
 }

1de8c166aeaa884504ab23c79fae59c2a61c9454
Aaron Piotrowski <aaron@artificialpixel.com>
2025-02-18 14:28:55 -0500

    Add initial Chapel Hill .NET website structure
    This commit introduces the foundational structure for the Chapel Hill .NET website, including Razor components, CSS, and initial pages like "About." It also adds project-specific configurations, assets, and a custom .gitignore file to manage ignored files effectively.

---
+++ chapelhilldotnet.web/Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace chapelhilldotnet.web.Models;
+
+public class Event
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+    public required string Location { get; set; }
+}

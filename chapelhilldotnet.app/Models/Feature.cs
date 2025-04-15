f0950a0e0909bc6c8a540530a48a69ad1728970f
Aaron Piotrowski <aaron@artificialpixel.com>
2025-04-15 14:48:39 -0400

    Remove Razor, CSS, and JavaScript assets from project
    This commit deletes various project files, including Razor imports, CSS styles, and a bundled Bootstrap JavaScript file. The changes may indicate cleanup, migration, or preparation for a refactor or replacement of these assets.

--- chapelhilldotnet.app/Models/Feature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace chapelhilldotnet.app.Models;
-
-public class Feature
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string IconColor { get; set; }
-    public string IconName { get; set; }
-}

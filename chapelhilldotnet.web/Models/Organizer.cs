52dfd6bda71895efc1c05e20ad2ae6a441a16914
Aaron Piotrowski <ap.piotrowski@outlook.com>
2026-03-04 12:50:20 -0500

    Enhance accessibility tests and model properties by adding required attributes and additional social media links

--- chapelhilldotnet.web/Models/Organizer.cs
+++ chapelhilldotnet.web/Models/Organizer.cs
@@ -3,10 +3,10 @@
 public class Organizer
 {
     public int Id { get; init; }
-    public string Name { get; init; }
-    public string Bio { get; init; }
-    public string ImageUrl { get; init; }
-    public string TwitterUrl { get; init; }
-    public string LinkedInUrl { get; init; }
-    public string GitHubUrl { get; init; }
+    public required string Name { get; init; }
+    public required string Bio { get; init; }
+    public required string ImageUrl { get; init; }
+    public required string TwitterUrl { get; init; }
+    public required string LinkedInUrl { get; init; }
+    public required string GitHubUrl { get; init; }
 }

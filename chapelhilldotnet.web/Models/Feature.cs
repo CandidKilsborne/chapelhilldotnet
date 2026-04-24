cc994c929d7212c64ef9434149633d1f48379dc3
Aaron Piotrowski <ap.piotrowski@outlook.com>
2026-04-24 15:28:28 -0400

    Remove unused `Feature` model, adjust component attributes, and refine private fields in `NavMenu.razor`.

--- chapelhilldotnet.web/Models/Feature.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace chapelhilldotnet.web.Models;
-
-public class Feature
-{
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string IconColor { get; set; }
-    public string IconName { get; set; }
-}
